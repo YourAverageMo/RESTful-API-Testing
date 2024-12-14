ed533ff98f843f3bdda3465e90dd3948b4df195b
YourAverageMo <Yilmaz@arizona.edu>
2024-12-13 22:09:30 -0700

    init testing framework

---
+++ tests/test_api.py
@@ -0,0 +1,18 @@
+import pytest
+from main import get_stock_price, Scraper
+
+
+def test_api():
+    pass
+
+
+def test_api_response_time():
+    pass
+
+
+def test_api_response_time_multi():
+    pass
+
+
+def test_api_invalid_inputs():
+    pass
